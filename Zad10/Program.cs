382e9dfc186bbf6e85fc25e3c5e6214d2bc58bf0
Vlad <isudmed@mail.ru>
2022-08-15 10:50:48 +0400

    first commit

---
+++ Zad10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число. введите выход для закрытия программы");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("выход"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
